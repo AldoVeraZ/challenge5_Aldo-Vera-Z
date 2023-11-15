396a4fdd51cd18aa6edb5f1cab5f471c37c72cbe
Aldo Esteban Vera Zúñiga <aestebanveraz@gmail.com>
2023-11-15 16:33:42 -0300

    fixing the exercise1

--- exercise1.js
+++ exercise1.js
@@ -15,32 +15,34 @@ Gato #9: 😹
 Gato #10: 😺
 ``` */
 
-console.log(/----Ejercicio #1 , ordinary version-------/);
-
-let numberOfcats = 4;
-
-for (let index = 1; index <= numberOfcats; index++) {
-  if (index === 3 || index === 6 || index === 9) {
-    console.log(`Gato #:${index} 😹`);
-  } else {
-    console.log(`Gato #:${index}  😺`);
-  }
-}
-
-console.log(/----Ejercicio #1 , Formal version-------/);
-
 const amountOfcats = 10;
 
-for (let c = 1; c <= amountOfcats; c++) {
-  /* encapsulo el mensaje con el numero de gato del ciclo en la variable cats */
-  const cats = `Gato #: ${c}`;
+for (let i = 1; i <= amountOfcats; i++) {
+  const mod = i % 3;
+  /* quiero saber en base al resto de la división (divisible por 3) de cada 3 gatos, va a ser que se me va a repetir el ciclo
+  al hacer i % 3, me va a decir en q posicion de ese ciclo de 3 voy a estar */
 
-  /* si c % 3 = 0, es impar */
-  if (c % 3 === 0) {
-    /* print joker cats */
-    console.log(`${cats} 😹`);
+  let cats = `Gato #${i}:`;
+  /* Entrega mensaje con el numero de gatos en posicion */
+
+  /* ver que devuelve el modulo en cada vuelta */
+  /*  console.log(modulo); */
+  /* i en la primera vuelta vale 1, entonces 1 / 3 da 0 si frenamos en el primer numero natural y cuanto sobro? 1 
+  segunda vuelta i = 2 , 2 / 3 = 0 no entra ninguna vez, entonces el resto tenemos 2 */
+  let emojiCats = cats;
+  /* se cera variable emojiCats para mejor comprensión, esta toma el valor del mensaje de gatos con su lugar para despues
+  concatenarles los emoji segun el if de abajo */
+
+  if (mod === 1) {
+    /* Si el resto o el residuo de la division de index modulo 3 es exactamente 1 mostrar el primer gato */
+    emojiCats = cats.concat("😺");
+    /* re asigno la variable cats llamandola con su valor respectivo de gato y lugar de gato para concatenarle el emoji */
+  } else if (mod === 2) {
+    /* Si el resto de la division de index modulo 3 segun el lugar que caiga , da exactamente 2 mostarr el segundo gato */
+    emojiCats = cats.concat("😸");
   } else {
-    /* print smiley cats */
-    console.log(`${cats} 😺`);
+    /* Si el resto de la division de index modulo 3 en el lugar que caiga da exactamente 0 es divisible por 3 y mostrar el tercer gato */
+    emojiCats = cats.concat("😹");
   }
+  console.log(emojiCats);
 }
